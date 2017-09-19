15d239fa4df816e82fbda414b9a092cc4520f227
Hack Reactor Student <student@hackreactor.com>
2017-09-18 20:38:10 -0700

    Implement working get and set messages with MYSQL database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,18 +5,15 @@ module.exports = {
     get: function (req, res) { // a function which handles a get request for all messages
       console.log('Serving request type ' + req.method + ' for url ' + req.url);
       console.log(req.body);
-      models.messages.get(function(messages) {
-        console.log(messages);
-        res.status(200).send(messages);
+      models.messages.get(function(error, messages) {
+        res.status(error ? 400 : 200).send(messages);
       });
     }, 
     post: function (req, res) { // a function which handles posting a message to the database
       console.log('Serving request type ' + req.method + ' for url ' + req.url);
       console.log(req.body);
-      //console.log(typeof req.body);
-      models.messages.post(req.body, function(err) {
-        var statusCode = err ? 400 : 201;
-        res.status(statusCode).send();
+      models.messages.post(req.body, function(error, results) {
+        res.status(error ? 400 : 201).send();
       });
     } 
   },
@@ -25,10 +22,15 @@ module.exports = {
     // Ditto as above
     get: function (req, res) {
       console.log('Serving request type ' + req.method + ' for url ' + req.url);         
-      
+      models.users.get(function(error, users) {
+        res.status(error ? 400 : 200).send(users);
+      });
     },
     post: function (req, res) {
-      console.log('Serving request type ' + req.method + ' for url ' + req.url);         
+      console.log('Serving request type ' + req.method + ' for url ' + req.url);
+      models.users.post(req.body, function(error, results) {
+        res.status(error ? 400 : 201).send();
+      });
     }
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -4,14 +4,24 @@ module.exports = {
   messages: {
     get: function (cb) { // a function which produces all the messages
       var queryAllMessages = 'SELECT messages.id, messages.message AS text, messages.roomname, users.name AS "username" FROM messages, users WHERE messages.user_id = users.id';
-      db.query(queryAllMessages, function(err, results, fields) {
-        if (err) { console.log(err); }
-        cb(results);
+      db.query(queryAllMessages, function(error, messages, fields) {
+        if (error) { console.log(error); }
+        cb(error, messages);
       });
     }, 
     post: function (data, cb) { // a function which can be used to insert a message into the database
-      module.exports.users.post({username: 'what'}, function(id) {
-        console.log(id);
+      module.exports.users.post(data, function(error, userId) {
+        if (!error) {
+          var newRow = {
+            'message': data.text,
+            'roomname': data.roomname,
+            'user_id': userId
+          };
+          db.query('INSERT INTO messages SET ?', newRow, function(error, results, fields) {
+            if (error) { console.log(error); }
+            cb(error, results);
+          });
+        }
       });
     } 
   },
@@ -20,24 +30,22 @@ module.exports = {
     // Ditto as above.
     get: function (cb) {
       var queryAllUsers = 'SELECT * FROM users';
-      db.query(queryAllUsers, function(err, results, fields) {
-        if (err) { console.log(err); }
-        cb(results);
+      db.query(queryAllUsers, function(error, users, fields) {
+        if (error) { console.log(error); }
+        cb(error, users);
       });
     },
     post: function (data, cb) {
       var queryUsername = 'SELECT * FROM users WHERE name = \'' + data.username + '\'';
-      // var insertUsername = 'INSERT INTO users SET '
-      db.query(queryUsername, function(err, results, fields) {
-        if (err) { console.log(err); }
+      db.query(queryUsername, function(error, results, fields) {
+        if (error) { console.log(error); }
         if (results.length === 0) {
-          db.query('INSERT INTO users SET ?', {name: data.username}, function(err, results, fields) {
-            if (err) { console.log(err); }
-            cb(results.insertId);
+          db.query('INSERT INTO users SET ?', {name: data.username}, function(error, results, fields) {
+            if (error) { console.log(error); }
+            cb(error, results.insertId);
           });
         } else {
-          console.log(results);
-          cb(results[0].id);
+          cb(error, results[0].id);
         }
       });
     }
