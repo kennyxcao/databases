a8d70afc24af45dcdeb568c45d65a3c402bbb854
Hack Reactor Student <student@hackreactor.com>
2017-09-19 15:25:55 -0700

    Implement two additional unit tests for GET and POST functions for users table

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -17,7 +17,6 @@ describe('Persistent Node Chat Server', function() {
     dbConnection.connect();
 
     var tablename = 'messages'; // TODO: fill this out
-
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
     dbConnection.query('truncate ' + tablename, done);
@@ -46,20 +45,16 @@ describe('Persistent Node Chat Server', function() {
       }, function () {
         // Now if we look in the database, we should find the
         // posted message there.
-
         // TODO: You might have to change this test to get all the data from
         // your message table, since this is schema-dependent.
         var queryString = 'SELECT * FROM messages';
         var queryArgs = [];
 
         dbConnection.query(queryString, queryArgs, function(err, results) {
-          console.log(results);
           // Should have one result:
           expect(results.length).to.equal(1);
-
           // TODO: If you don't have a column named text, change this test.
           expect(results[0].message).to.equal('In mercy\'s name, three days is all I need.');
-
           done();
         });
       });
@@ -73,16 +68,45 @@ describe('Persistent Node Chat Server', function() {
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
-
     dbConnection.query(queryString, queryArgs, function(err) {
       if (err) { throw err; }
-
       // Now query the Node chat server and see if it returns
       // the message we just inserted:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
         expect(messageLog[0].text).to.equal('Men like you can never change!');
         expect(messageLog[0].roomname).to.equal('main');
+        done();
+      });
+    });
+  });
+  
+  it('Should POST and retrive data from db', function(done) {
+    // Post the user to the chat server.
+    request({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/users',
+      json: { username: 'Kenny & Kevin' }
+    }, function () {
+      var queryString = 'SELECT * FROM users';
+      var queryArgs = [];
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        var users = results.map(user => user.name);
+        expect(users.includes('Kenny & Kevin')).to.be.true;
+        done();
+      });
+    });
+  });
+  
+  it('Should output all users from the DB', function(done) {
+    request({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/users',
+      json: { username: 'Hacker' }
+    }, function () {    
+      request('http://127.0.0.1:3000/classes/users', function(error, response, body) {
+        var users = JSON.parse(body).map(user => user.username);
+        expect(users.includes('Hacker')).to.be.true;
         done();
       });
     });
